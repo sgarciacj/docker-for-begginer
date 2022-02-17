46d2305a7b3d0f1413264f43207ad350ba5d7f9c
Sandy Garcia <94652165+sgarciacj@users.noreply.github.com>
2022-02-16 21:23:20 -0400

    Add Example Dockerfile
    Add Example Dockerfile a serverjs and Dockerfile

---
+++ DockerfileExample/server.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const os = require('os');
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// Info about task
+const datetime = new Date();
+
+const message_info = `Docker Training | Homework 2
+ - Server running at http://${HOST}:${PORT}/
+ - TimeZone: ${Intl.DateTimeFormat().resolvedOptions().timeZone}
+ - Offset: ${datetime.getTimezoneOffset()}
+ - Date: ${datetime.toLocaleDateString()}
+ - Time: ${datetime.toLocaleTimeString()}
+ - Hostname: ${os.hostname}
+ - IP Address: `;
+
+
+var net_int = os.networkInterfaces();
+var no_of_network_interfaces = 0;
+
+// Review Network (IP address available)
+var network_info = '';
+for (var key in net_int) {
+        network_info += `\n\t${key}`;
+        var net_infos=net_int[key];
+     net_infos.forEach(element => {
+        no_of_network_interfaces++;
+    for (var attr in element){
+                network_info += `\n\t\t${attr} : ${element[attr]}`;
+    }
+  });
+}
+
+// App
+const app = express();
+app.get('/info/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(message_info + network_info);
+  res.send(message_info + network_info);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Endpoint http://${HOST}:${PORT}/info/`);
